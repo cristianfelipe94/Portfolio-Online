15174ec5e55bf8edef652c1749f119d7e963fed4
Cristian Felipe <cristianpipe0494@gmail.com>
2019-04-15 10:38:50 -0600

    First commit, have initialized repo.

---
+++ js/getterElements.js
@@ -0,0 +1,31 @@
+// Get elements from DOM.
+// ///////////////////////
+const bodyDom = document.querySelector('body');
+const helloBtn = document.getElementById('js-img-avatar');
+const landingPage = document.getElementById('js-landingpage');
+const VerticalTabs = document.querySelectorAll('#js-tabs-info-wrapper');
+const btnClose = document.getElementById('js-btn-close');
+const homeContentBlock = document.getElementById('js-home-page');
+const portContentBlock = document.getElementById('js-port-page');
+const projectsWrapper = document.getElementById('js-projects-wrapper');
+
+const educationContentBlock = document.getElementById('js-diploma-page');
+
+const backgroundImg = document.getElementById('js-background-img');
+
+const backgroundModal = document.getElementById('js-modal');
+const closeModalBtn = document.getElementById('js-close-modal');
+
+const langWrapperDisplay = document.getElementById('js-lang-wrapper');
+const socialWrapperDisplay = document.getElementById('js-social-wrapper');
+const signWrapperDisplay = document.getElementById('js-sign-wrapper');
+
+const portFolioContent = document.getElementById('js-wrapper-portfolio');
+
+const tabsDom = document.getElementsByClassName('tab-items');
+
+const activityContentBlock = document.getElementById('js-activity-list');
+
+const btnSizer = document.getElementById('js-btn-size');
+const sizeName = document.getElementById('js-size-name');
+// ///////////////////////
